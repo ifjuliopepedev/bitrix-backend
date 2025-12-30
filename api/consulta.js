afa13dbcba3141ce63ce8891c78611d947f35af1
ifjuliopepedev <ifjuliopepe@gmail.com>
2025-12-29 20:59:39 -0400

    Update consulta.js

--- api/consulta.js
+++ api/consulta.js
@@ -3,6 +3,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Content-Language", "pt-BR");
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
 
   if (req.method === "OPTIONS") return res.status(200).end();
 
